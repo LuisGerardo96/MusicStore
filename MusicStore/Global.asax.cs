7d756f41917b8c5dcb21ba04bb2a0bc86d5e2ef8
Administrador <Administrador@ALEESG>
2020-01-22 13:09:31 -0600

    Music Store

---
+++ MusicStore/Global.asax.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+
+namespace MusicStore
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //inicializamos una base de datos de prueba 
+            Database.SetInitializer(
+            new Models.SampleData());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
