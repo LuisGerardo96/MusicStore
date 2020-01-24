eb1629d7c4933536029895389b4a8f744ebc00fc
Administrador <Administrador@ALEESG>
2020-01-24 14:57:43 -0600

    concurrency fix

--- MusicStore/Controllers/StoreManagerController.cs
+++ MusicStore/Controllers/StoreManagerController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MusicStore.Models;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
 
 namespace MusicStore.Controllers
 {
@@ -62,27 +64,33 @@ namespace MusicStore.Controllers
             ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
             return View(album);
         }
-
+      
+            
         // GET: StoreManager/Edit/5
-        public ActionResult Edit(int? id)
+        public async Task<ActionResult> Edit(int? id)
         {
+            string[] fieldsToBind = new string[] { "GenreId", "ArtistId", "Title", "Price", "AlbumArtURL", "RowVersion" };
+       
+            
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Album album = db.Albums.Find(id);
-            if (album == null)
+            var albumupdate = await db.Albums.FindAsync(id);
+            if (albumupdate == null)
             {
-                return HttpNotFound();
+                Album albundeleted = new Album();
+                TryUpdateModel(albundeleted, fieldsToBind);
+                ModelState.AddModelError(string.Empty, "Unable to save changes. The department was deleted by another user.");
+                ViewBag.GenreID = new SelectList(db.Genres, "GenreId", "Name", albundeleted.GenreId);
+                ViewBag.ArtistID = new SelectList(db.Artists, "ArtistId", "Name", albundeleted.ArtistId);
+                return View(albundeleted);
             }
-            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
-            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
-            return View(album);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", albumupdate.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", albumupdate.GenreId);
+            return View(albumupdate);
         }
 
-        // POST: StoreManager/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "AlbumId,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
@@ -93,13 +101,73 @@ namespace MusicStore.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
             ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
             return View(album);
         }
+            //public async Task<ActionResult> EditAsync([Bind(Include = "AlbumId,GenreId,ArtistId,Title,Price,AlbumArtUrl,RowVersion")] Album album,byte [] rowVersion)
+            //{
+            //    string[] fieldsToBind = new string[] { "GenreId", "ArtistId", "Title", "Price", "AlbumArtURL", "RowVersion" };
+            //    if (TryUpdateModel(album,fieldsToBind))
+            //    {
+            //        try
+            //        {
+            //            db.Entry(album).OriginalValues["RowVersion"] = rowVersion;
+            //            await db.SaveChangesAsync();
+            //            return RedirectToAction("Index");
+            //        }
+            //        catch (DbUpdateConcurrencyException ex)
+            //        {
+            //            var entry = ex.Entries.Single();
+            //            var clientValues = (Album)entry.Entity;
+            //            var databaseEntry = entry.GetDatabaseValues();
+            //            if (databaseEntry == null)
+            //            {
+            //                ModelState.AddModelError(string.Empty,
+            //                    "Unable to save changes. The department was deleted by another user.");
+            //            }
+            //            else
+            //            {
+            //                var databaseValues = (Album)databaseEntry.ToObject();
 
-        // GET: StoreManager/Delete/5
-        public ActionResult Delete(int? id)
+            //                if (databaseValues.GenreId != clientValues.GenreId)
+            //                    ModelState.AddModelError("Genre", "Current value: "
+            //                        + databaseValues.GenreId);
+            //                if (databaseValues.ArtistId != clientValues.ArtistId)
+            //                    ModelState.AddModelError("Artist", "Current value: "
+            //                        + databaseValues.ArtistId);
+            //                if (databaseValues.Title != clientValues.Title)
+            //                    ModelState.AddModelError("Title", "Current value: "
+            //                        + databaseValues.Title);
+            //                if (databaseValues.Price != clientValues.Price)
+            //                    ModelState.AddModelError("Price", "Current value: "
+            //                        + databaseValues.Price);
+            //                if (databaseValues.AlbumArtUrl != clientValues.AlbumArtUrl)
+            //                    ModelState.AddModelError("AlbumArtUrl", "Current value: "
+            //                        + databaseValues.AlbumArtUrl);
+            //                ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+            //                    + "was modified by another user after you got the original value. The "
+            //                    + "edit operation was canceled and the current values in the database "
+            //                    + "have been displayed. If you still want to edit this record, click "
+            //                    + "the Save button again. Otherwise click the Back to List hyperlink.");
+            //                album.RowVersion = databaseValues.RowVersion;
+            //            }
+            //        }
+            //        catch (RetryLimitExceededException /* dex */)
+            //        {
+            //            //Log the error (uncomment dex variable name and add a line here to write a log.)
+            //            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            //        }
+            //    }
+            //    ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            //    ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            //    return View(album);
+            //}
+
+
+
+            // GET: StoreManager/Delete/5
+            public ActionResult Delete(int? id)
         {
             if (id == null)
             {
