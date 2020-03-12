514977d2f7f85e9feb475d837f572119668d7153
Administrador <Administrador@ALEESG>
2020-03-12 15:20:48 -0600

    dropwdown validation -create view

---
+++ MusicStore/Scripts/Deleteconfim.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('a').click(function () { // noting work => btn btn-small btn-danger
+
+        if (confirm("Etes vous sur de vouloir effacer cet element ?"))
+            alert("confirm ok ");
+    });
+});
---
+++ MusicStore/Scripts/Dropdownvalidation.js
@@ -0,0 +1,9 @@
+﻿function ddlValidate() {
+    var e = document.getElementById("GenreId");
+    var optionSelIndex = e.options[e.selectedIndex].value;
+    console.log(optionSelIndex)
+    if (optionSelIndex <= 0 || optionSelIndex > 10) {
+        alert("Value not permit ");
+        return false
+    }
+}
